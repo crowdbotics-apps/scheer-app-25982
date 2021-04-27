b4d67af6782cf9d1c752a5d254e6e12131f42461
Crowdbotics <team@crowdbotics.com>
2021-04-27 21:36:31 +0000

    Update models

--- backend/scheer_app_25982/wsgi.py
+++ backend/scheer_app_25982/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'scheer_app_25982.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "scheer_app_25982.settings")
 
 application = get_wsgi_application()
